6c149f360ada13489b4d3cba10f3c90a02608bb8
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-11-04 18:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function Car(make, model) {
 	
 }
 Car.prototype.getMakeModel=function(){
-		return ${this.make}${this.model};
+		return `${this.make}${this.model}`;
 	}
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
@@ -17,7 +17,7 @@ SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
 
 SportsCar.prototype.topSpeed=function(){
-	return ${this.topSpeed};
+	return `${this.topSpeed}`;
 }
 // Do not change the code below
 window.Car = Car;
